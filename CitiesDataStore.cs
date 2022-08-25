37927ef24546b4714d6b134ceeb23d483beb9f5a
Daniel Irungu <irungu214@gmail.com>
2022-08-26 01:04:27 +0300

    add controllers and dto

---
+++ CitiesDataStore.cs
@@ -0,0 +1,48 @@
+﻿using CityInfo.API.Models;
+using System.Xml.Linq;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>() {
+            new CityDto()
+        {
+            Id = 1,
+                Name = "Nairobi",
+                Description = "City in the sun"
+
+        },
+            new CityDto()
+        {
+            Id = 2,
+                Name = "Mombasa",
+                Description = "Ocean tides"
+
+        },
+            new CityDto()
+        {
+            Id = 3,
+                Name = "Kisumu",
+                Description = "Samaki tu"
+
+        },
+            new CityDto()
+        {
+            Id = 4,
+                Name = "Paris",
+                Description = "Eiffel tower"
+
+        }
+    };
+
+        }
+
+    }
+}
